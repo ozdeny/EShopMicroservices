3b0c0a69e314902289a7d951a320262a58332866
Yakup ÖZDEN <yakup.ozden@adesso.com.tr>
2024-08-22 00:03:47 +0300

    Initial version for the Catalog Microservice

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,16 @@
+﻿using Carter;
+
+namespace Catalog.API.Products.CreateProduct;
+
+
+public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+
+public record CreateProductResponse(Guid Id);
+
+public class CreateProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        
+    }
+}
