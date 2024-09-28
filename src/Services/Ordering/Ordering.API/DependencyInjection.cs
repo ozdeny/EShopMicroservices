791044246cde9e96a0705333cabec62ddfa2e6db
Yakup ÖZDEN <ozdeny@itu.edu.tr>
2024-09-28 15:06:05 +0300

    Ordering EF Core interceptor etc.

--- src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -13,6 +13,7 @@ public static class DependencyInjection
     {
         // app.MapCarter();
 
+
         return app;
     }
 }
--- src/Services/Ordering/Ordering.Domain/Models/Order.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -26,7 +26,23 @@ public class Order : Aggregate<OrderId>
         get => OrderItems.Sum(x => x.Price * x.Quantity);
         private set { }
     }
+    public static Order Create(OrderId id, CustomerId customerId, OrderName orderName, Address shippingAddress, Address billingAddress, Payment payment)
+    {
+        var order = new Order
+        {
+            Id = id,
+            CustomerId = customerId,
+            OrderName = orderName,
+            ShippingAddress = shippingAddress,
+            BillingAddress = billingAddress,
+            Payment = payment,
+            Status = OrderStatus.Pending
+        };
 
+        order.AddDomainEvent(new OrderCreatedEvent(order));
+
+        return order;
+    }
 
     public static Order Create(OrderId id, CustomerId customerId, OrderName orderName, Address shippingAddress,
         Address billingAddress, Payment payment, OrderStatus status)
